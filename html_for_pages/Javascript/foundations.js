4fcdf2c2d6e3df6c0d4f6a8cd2a0bc338757dc89
dee-me-tree-or-love <dmitry-orlov@hotmail.com>
2016-03-13 01:02:38 +0100

    MeNeither!

---
+++ html_for_pages/Javascript/foundations.js
@@ -0,0 +1,12 @@
+function DisplayMore1()
+{
+	document.getElementById(ed1).style.overflow = 'visible';
+	document.getElementById(ed1).style.opacity = 0.7;
+}
+
+function Alerted()
+{
+
+window.alert(5 + 6);
+
+}
