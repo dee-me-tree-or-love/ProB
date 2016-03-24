cfb56a21c59d978c5f8c052b191d50425bab098a
dee-me-tree-or-love <dmitry-orlov@hotmail.com>
2016-03-24 17:37:18 +0100

    Still trying to make registration more fancy

--- html_for_pages/PHPFUNC/REGISTER.php
+++ html_for_pages/PHPFUNC/REGISTER.php
@@ -1,14 +1,71 @@
 <?php
 include 'DBCONNECT.php';
 
+$errorStat = 0;
 
-$error = '';
+
+
+// to show an alert and go back to the same page
+// BEGIN
+function SendError($message)
+{
+    // makes an error status equal to 1
+    $errorStat = 1;
+    // To show alert
+    echo "<script type='text/javascript'> alert('{$message}'); window.location.replace('../signup.php'); </script>";
+    // end
+    
+    // send back to location
+    //header("Location: ../signup.php"); 
+}
+// END
+
+
+//BEGIN
+
+//    // sql to check the db
+//    $sql = 'SELECT email FROM proBusers WHERE email = :email';
+//    $emver = $db->prepare($sql);
+//    $emver->execute(
+//            [
+//                ':email' => $_POST['email'] 
+//            ]
+//        );
+//    
+//    
+//    
+//        // gets an array from the 
+//    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    //echo "<script type='text/javascript'> alert('{$row}'); window.location.replace('../signup.php'); </script>";
+    //if(!empty($row['email'])){
+            //SendError('E-Mail provided is already in use. Please log in.');
+            // to open log in
+            //echo '<script type="text/javascript"> ShowAccountEnter(); </script>';
+            // end
+        //}
+
+//END
+
 
 // VALIDATION, PASSWORD VERIFICATION
 if($_POST['password'] != $_POST['password_confirm']){
-  SendError('Passwords do not match');
-  
-} else {
+   
+//    $error = 'fuck';
+//    echo "<script type='text/javascript'> alert('{$error}'); </script>";
+    // OUTPUTS AN ERROR, MAKES ERROR INDICATOR 1 and redirects back to the sign up page    
+    SendError("Passwords do not match");
+} 
+if(strlen($_POST['password']) < 3)
+{
+    SendError("Password is too short");
+}
+
+
+
+// only if nothing happened and errorStatus remained 0 we can process
+if($errorStat === 0)
+  //else
+      {
     
     // if everything is okay, do this: 
     $sql = 'INSERT INTO proBusers (email,password,subscription) VALUES (:email, :password, :subscription)';
@@ -40,14 +97,6 @@ if($_POST['password'] != $_POST['password_confirm']){
 
   
     
-function SendError($message)
-{
-    
-    $error = $message;
-   
-    echo $error;
-    
-    header("Location: ../signup.php");
-}
+
 
 ?>
