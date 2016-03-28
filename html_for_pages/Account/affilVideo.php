5f73f47f2044ca10e97616d2bfb87f2ada5eac55
dee-me-tree-or-love <dmitry-orlov@hotmail.com>
2016-03-28 13:03:07 +0200

    account page layout

---
+++ html_for_pages/Account/affilVideo.php
@@ -0,0 +1,10 @@
+
+<!--// will store the video(s) for affiliate programs-->
+
+   <iframe src="https://player.vimeo.com/video/6047270" width="500" height="281" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+    <p><a href="https://vimeo.com/6047270">WWF campaign</a> 
+        from <a href="https://vimeo.com/groovysushi">
+        Desrumaux Celine</a> on <a href="https://vimeo.com">Vimeo</a>.
+        <a href="https://www.worldwildlife.org/">(WWF.org)</a>
+    </p>
+
--- html_for_pages/account.php
+++ html_for_pages/account.php
@@ -17,87 +17,96 @@
 
 <!-- Main -->
 <div id="Content">
-<div id="PersonalStatistics">
-<div id="Personal_Details">
-<h1>Personal Details</h1>
-<div class="form-group">
-            <label for="first_name">First Name</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="text" name="first_name" id="first_name" />
-          </div>
-<div class="form-group">
-            <label for="last_name">Last Name</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="text" name="last_name" id="last_name" />
-          </div>
-<div class="form-group">
-            <label for="e_mail">E- Mail</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="email" name="e_mail" id="e_mail" />
-          </div>
-<div class="form-group">
-            <label for="phone">Phone</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="number" name="phone" id="phone" />
-          </div>
-<form action="">
-<input type="checkbox" name="Newsletter ProB" value="Newsletter ProB">I want to receive the Project Banana newsletter and emails about other Project Banana campaigns<br>
-</form>
-<form action="">
-<input type="submit" value="Save Changes" class="btn" id="save_btn"/>
-</form>
-</div>
-<hr>
-<div id="Statistics">
-<h1>Statistics</h1>
-<div class="Statistics">
-            <label for="your_donations">Your Donations</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="number" name="your_donation" id="your_donation" readonly/>
-          </div>
-<div class="Statistics">
-            <label for="your_donations">Shares</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="number" name="shares" id="shares" readonly/>
-          </div>
-<div class="Statistics">
-            <label for="your_donations">Total Donations</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="number" name="total_donations" id="total_donations" readonly/>
-          </div>
-<div class="Statistics">
-            <label for="your_donations">Total Shares</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="number" name="total_shares" id="total_shares" readonly/>
-          </div>
-        </div>
-      </div>
-<hr>
-<div id="ActionDonate">
-<h1>Take Action</h1>
-<img src="" alt="" style="width:400px;height:35px;"><h4>Support</h4><br>
-<img src="" alt="" style="width:400px;height:35px;"><h4>Support</h4><br>
-<img src="" alt="" style="width:400px;height:35px;"><h4>Support</h4><br>
-<img src="" alt="" style="width:400px;height:35px;"><h4>Support</h4><br>
-<img src="" alt="" style="width:400px;height:35px;"><h4>Support</h4><br>
-<hr>
-<div id="Donate">
-<h1>Donate</h1>
-</div>
-<div class="form-group">
-            <label for="">Card Holder's Name</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="text" name="card_holder's_name" id="card_holder's_name" />
-          </div>
-<div class="form-group">
-            <label for="">Credit Card's Number</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="text" name="credit_card_number" id="credit_card_number" />
-          </div>
-<div class="form-group">
-            <label for="">Card's CVV</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="text" name="card_CVV" id="card_CVV" />
-          </div>
-<div class="form-group">
-            <label for="">Card's Expiration Date</label>
-            <input autofocus="autofocus" class="form-control" required="required" type="month" name="month" id="month" />
-          </div>
-<p>
-<input type="submit" name="DONATE" value="DONATE" class="btn" id="donate_btn"/>
 
-</p>
+    
+<div id="DetStats" class="horizontal">    
+    <div id="Personal_Details" class="sect left">
+        <h1>Personal Details</h1>
+        <div class="form-group">
+                    <label for="first_name">First Name</label>
+                    <input autofocus="autofocus" class="form-control" required="required" type="text" name="first_name" id="first_name" />
+                  </div>
+        <div class="form-group">
+                    <label for="last_name">Last Name</label>
+                    <input autofocus="autofocus" class="form-control" required="required" type="text" name="last_name" id="last_name" />
+                  </div>
+        <div class="form-group">
+                    <label for="e_mail">E- Mail</label>
+                    <input autofocus="autofocus" class="form-control" required="required" type="email" name="e_mail" id="e_mail" />
+                  </div>
+        <div class="form-group">
+                    <label for="phone">Phone</label>
+                    <input autofocus="autofocus" class="form-control" required="required" type="number" name="phone" id="phone" />
+                  </div>
+        <form action="">
+        <input type="checkbox" name="Newsletter ProB" value="Newsletter ProB">
+            I want to receive the Project Banana newsletter and emails about 
+            other Project Banana campaigns<br>
+        </form>
+        <form action="">
+        <input type="submit" value="Save Changes" class="btn" id="save_btn"/>
+        </form>
+    </div>
+
+    <div id="stats" class="sect right">
+            <h1>Statistics</h1>
+        <div class="Statistics">
+                <label for="your_donations">Your Donations</label>
+                <input autofocus="autofocus" class="form-control" required="required" type="number" name="your_donation" id="your_donation" readonly/>
+              </div>
+        <div class="Statistics">
+                <label for="your_donations">Video Views</label>
+                <input autofocus="autofocus" class="form-control" required="required" type="number" name="shares" id="shares" readonly/>
+              </div>
+        <div class="Statistics">
+                <label for="your_donations">Total Donations</label>
+                <input autofocus="autofocus" class="form-control" required="required" type="number" name="total_donations" id="total_donations" readonly/>
+              </div>
+        <div class="Statistics">
+                <label for="your_donations">Total Video Views</label>
+                <input autofocus="autofocus" class="form-control" required="required" type="number" name="total_shares" id="total_shares" readonly/>
+              </div>
+    </div>
 </div>
 
+    
+    
+<div id="ActionDonate" class="horizontal">
+    <div id="afilVid" class="sect left">
+        <h1>Take Action</h1>
+            <?php require_once 'account/affilVideo.php';?>
+    
+    </div>
+    <div id="Donate" class="sect right">
+        <h1>Donate</h1>
+
+        <div class="form-group">
+                    <label for="">Card Holder's Name</label>
+                    <input autofocus="autofocus" class="form-control" required="required" type="text" name="card_holder's_name" id="card_holder's_name" />
+                  </div>
+        <div class="form-group">
+                    <label for="">Credit Card's Number</label>
+                    <input autofocus="autofocus" class="form-control" required="required" type="text" name="credit_card_number" id="credit_card_number" />
+                  </div>
+        <div class="form-group">
+                    <label for="">Card's CVV</label>
+                    <input autofocus="autofocus" class="form-control" required="required" type="text" name="card_CVV" id="card_CVV" />
+                  </div>
+        <div class="form-group">
+                    <label for="">Card's Expiration Date</label>
+                    <input autofocus="autofocus" class="form-control" required="required" type="month" name="month" id="month" />
+                  </div>
+        <p>
+        <input type="submit" name="DONATE" value="DONATE" class="btn" id="donate_btn"/>
+        </p>
+
+    </div>
+</div>
+
+
+
+</div>
+<!-- end contents -->
 
 <!-- Footer -->
 	<div id = "footer">
